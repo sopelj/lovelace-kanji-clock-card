46ca172447cd86a91483a5325ee60e5ae4a71424
Jesse Sopel <jesse.sopel@gmail.com>
2020-12-19 21:39:28 -0500

    Add country code to hacs.json

--- src/const.ts
+++ src/const.ts
@@ -1,4 +1,4 @@
-export const CARD_VERSION = '0.1.1';
+export const CARD_VERSION = '0.1.2';
 export const KANJI_NUMBERS = '霊一二三四五六七八九';
 export const KANJI_WEEKDAYS = '日月火水木金土';
 export const TENS = ['', '十', '百', '千'];
