f057c0dfaef55173fdfa7da767c6575b730e7949
Jesse Sopel <jesse.sopel@gmail.com>
2024-12-05 19:31:44 -0500

    feat: Bump version

--- src/const.ts
+++ src/const.ts
@@ -1,4 +1,4 @@
-export const CARD_VERSION = "1.1.0";
+export const CARD_VERSION = "1.2.0";
 export const KANJI_NUMBERS = "霊一二三四五六七八九";
 export const KANJI_WEEKDAYS = "日月火水木金土";
 export const TENS = ["", "十", "百", "千", "万"];
